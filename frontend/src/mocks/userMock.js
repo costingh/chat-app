6b0b4569b6685544ad187519abe03b7872f0b9b7
costingh <cgheorghe0702@stud.acs.upb.ro>
2021-09-26 12:52:04 +0300

    added userListMock

---
+++ frontend/src/mocks/userMock.js
@@ -0,0 +1,60 @@
+export const userMockList = [
+    {
+        profilePicture: 'https://randomuser.me/api/portraits/thumb/men/74.jpg',
+        username: 'Bessie Cooper',
+        lastMessage: 'Yes, I need your help with the project, it need it done by tomorrow 😫',
+        status: 'online'
+    },
+    {
+        profilePicture: 'https://randomuser.me/api/portraits/thumb/women/56.jpg',
+        username: 'Jenny Smith',
+        lastMessage: 'Perfect, thanks !',
+        status: 'online'
+    },
+    {
+        profilePicture: 'https://randomuser.me/api/portraits/thumb/women/17.jpg',
+        username: 'Courtney Simmons',
+        lastMessage: 'Going home soon, don\'t worry',
+        status: 'online'
+    },
+    {
+        profilePicture: 'https://randomuser.me/api/portraits/thumb/women/39.jpg',
+        username: 'Martha Curtis',
+        lastMessage: 'Great 😂',
+        status: 'online'
+    },
+    {
+        profilePicture: 'https://randomuser.me/api/portraits/thumb/men/27.jpg',
+        username: 'Gab Ryan',
+        lastMessage: 'Sure, may I get your phone number? 😃',
+        status: 'offline'
+    },
+    {
+        profilePicture: 'https://randomuser.me/api/portraits/thumb/men/17.jpg',
+        username: 'Jules Zimmermann',
+        lastMessage: 'Well, here I am, coming as faaast as I can !',
+        status: 'offline'
+    },
+    {
+        profilePicture: 'https://randomuser.me/api/portraits/thumb/men/9.jpg',
+        username: 'Mark Reid',
+        lastMessage: 'Have you listened to the latest album? Pure perfection',
+        status: 'offline'
+    },
+    {
+        profilePicture: 'https://randomuser.me/api/portraits/thumb/men/54.jpg',
+        username: 'Russell Williams',
+        lastMessage: 'Nice to meet you again',
+        status: 'offline'
+    },
+    {
+        profilePicture: 'https://randomuser.me/api/portraits/thumb/women/85.jpg',
+        username: 'Savannah Nguyen',
+        lastMessage: 'Really ?!',
+        status: 'offline'
+    },
+];
+
+export default {
+    userMockList,
+}

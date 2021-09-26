3e2aa0f09340f58d3b54d891daea7095904366c5
costingh <cgheorghe0702@stud.acs.upb.ro>
2021-09-26 10:50:35 +0300

    Added websockets for server

---
+++ server/src/main/java/com/web/chat/controllers/WebSocketTextController.java
@@ -0,0 +1,23 @@
+package com.web.chat.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+public class WebSocketTextController {
+
+    @Autowired
+    SimpMessagingTemplate template;
+
+    @MessageMapping("/send/{conversationId}")
+    @SendTo("/topic/{conversationId}")
+    public String sendMessage(@Payload String message) {
+        return message;
+    }
+}

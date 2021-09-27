30a4c3776425dac9c4db3c9ee922e91aa704dbba
costingh <cgheorghe0702@stud.acs.upb.ro>
2021-09-27 14:20:25 +0300

    remove @PreAuthorize from ConversationsController

--- server/src/main/java/com/web/chat/controllers/ConversationsController.java
+++ server/src/main/java/com/web/chat/controllers/ConversationsController.java
@@ -7,10 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,26 +26,26 @@ public class ConversationsController {
     private MongoTemplate mongoTemplate;
 
     @GetMapping("/all")
-    @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
     public List<Conversations> allConversations() {
         return conversationRepository.findAll();
     }
 
     @PostMapping("/add")
-    @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
     public Conversations addConversation(@RequestBody ConversationRequest conversation) {
         Conversations conversations = new Conversations(
                 conversation.getParticipantOneId(),
                 conversation.getParticipantTwoId()
         );
 
+        Date createdAt = new Date();
+        conversations.setCreatedDate(createdAt);
+
         conversationRepository.save(conversations);
         return conversations;
     }
 
     @GetMapping("/all/{userId}")
-    @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
-    public List<Conversations> addConversation(@PathVariable Conversations userId) {
+    public List<Conversations> addConversation(@PathVariable String userId) {
         Query query = new Query();
         List<Criteria> criteria = new ArrayList<>();
 
@@ -53,21 +53,19 @@ public class ConversationsController {
         criteria.add(Criteria.where("participantOneId").is(userId));
         criteria.add(Criteria.where("participantTwoId").is(userId));
 
-        query.addCriteria(new Criteria().andOperator(criteria.toArray(new Criteria[criteria.size()])));
+        query.addCriteria(new Criteria().orOperator(criteria.toArray(new Criteria[criteria.size()])));
 
         List<Conversations> conversations = mongoTemplate.find(query, Conversations.class);
         return conversations;
     }
 
     @GetMapping("/{conversationId}")
-    @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
     public Optional<Conversations> getConversation(@PathVariable String conversationId) {
         Optional<Conversations> conversations = conversationRepository.findById(conversationId);
         return conversations;
     }
 
     @DeleteMapping("/delete/{conversationId}")
-    @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
     public String deleteConversation(@PathVariable Conversations conversationId) {
         conversationRepository.delete(conversationId);
         return "Conversation has been deleted successfully!";

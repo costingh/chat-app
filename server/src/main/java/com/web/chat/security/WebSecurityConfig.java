66e5a9983a15f9fdf36987c933763e839994aafc
costingh <cgheorghe0702@stud.acs.upb.ro>
2021-09-30 11:27:17 +0300

    fixed WebSecurityConfig

--- server/src/main/java/com/web/chat/security/WebSecurityConfig.java
+++ server/src/main/java/com/web/chat/security/WebSecurityConfig.java
@@ -57,7 +57,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests().antMatchers("/api/auth/**").permitAll()
-                .antMatchers("/api/test/**", "/ws-message/**", "/api/conversation/**", "/api/messages/**").permitAll()
+                .antMatchers("/api/test/**", "/ws-message/**").permitAll()
                 .anyRequest().authenticated();
 
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
